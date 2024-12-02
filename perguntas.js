0cae511c13d8c89475dff85331406890c335ebb9
Andreoli123 <00001100458712sp@al.educacao.sp.gov.br>
2024-12-02 15:48:34 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
-)
-
-criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
-)
-
-criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+     'Lingua inglesa',
+    'Como se diz cama em Inglês?',
+    'cama em ingles é bed'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Como se diz chuva em Inglês?',
+    'chuva em ingles é rain'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz maçã em Inglês?',
+    'maçã em ingles é apple'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz tchau em Inglês?',
+    'Tchau em ingles é Bye'
 )
